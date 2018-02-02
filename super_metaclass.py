ee753aaa761c0a301d38dc168d98934748c0105e
matthieu <matthieu@treussart.com>
2018-02-02 04:24:00 +0100

    Test metaclass

---
+++ super_metaclass.py
@@ -0,0 +1,42 @@
+
+class MetaPeople(type):
+    def __new__(cls, *args, **kwargs):
+        print("Created Class {}".format(cls.__name__))
+        return type.__new__(cls, *args, **kwargs)
+
+    def __init__(self, *args, **kwargs):
+        print("Initialized Class {}".format(self.__class__))
+        type.__init__(self, *args, **kwargs)
+
+
+class Dad(metaclass=MetaPeople):
+    def __new__(cls, username):
+        print("Created Class {0}, username: {1}".format(cls.__name__, username))
+        return object.__new__(cls)
+
+    def __init__(self, username):
+        print("Initialized Class {}".format(self.__class__))
+        self.username = username
+
+    def __str__(self):
+        return self.username
+
+
+class Mum(metaclass=MetaPeople):
+    def __new__(cls, username, skills):
+        print("Created Class {0}, username: {1}".format(cls.__name__, username))
+        return object.__new__(cls)
+
+    def __init__(self, username, skills):
+        print("Initialized Class {}".format(self.__class__))
+        self.username = username
+        self.skills = skills
+
+    def __str__(self):
+        return self.username + str(self.skills)
+
+
+dad = Dad("daddy")
+print(dad)
+mum = Mum("mummy", ['play', 'humor'])
+print(mum)
