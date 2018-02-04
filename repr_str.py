6f6ff972780428efeb05ed1b6d3438d23b2fe723
matthieu <matthieu@treussart.com>
2018-02-04 20:49:04 +0100

    Update tests

--- repr_str.py
+++ repr_str.py
@@ -25,3 +25,29 @@ class Sic(object):
 print(str(Sic()))
 
 print(repr(Sic()))
+
+
+class Sic(object):
+    def __init__(self, x=0, y=0, prout="test"):
+        self.x = x
+        self.y = y
+        self.prout = prout
+
+    def __repr__(self):
+        keys = sorted(self.__dict__)
+        items = ("{}={!r}".format(k, self.__dict__[k]) for k in keys)
+        return "{}({})".format(type(self).__name__, ", ".join(items))
+
+    def run_instance(self):
+        pass
+
+    @classmethod
+    def run_class(cls):
+        pass
+
+    @staticmethod
+    def run_static():
+        pass
+
+
+print(str(Sic(5, 7)))
